2952e57ca9dd13265dae8651144ff2880e606e46
Erik Hanson <erik@eahanson.com>
2023-11-25 11:13:29 -0800

    Add `Supra.limit` and `@type result`

--- lib/supra.ex
+++ lib/supra.ex
@@ -1,9 +1,20 @@
 defmodule Supra do
+  # @related [test](test/supra_test.exs)
   @moduledoc """
   Documentation for `Supra`.
   """
 
+  require Ecto.Query
+
+  @type result(type) :: {:ok, type} | {:error, Ecto.Changeset.t(type)}
+  @type result(ok_t, error_t) :: {:ok, ok_t} | {:error, Ecto.Changeset.t(error_t)}
+
   @spec count(Ecto.Queryable.t(), repo: Ecto.Repo.t()) :: non_neg_integer()
   def count(queryable, repo: repo),
     do: queryable |> repo.aggregate(:count)
+
+  @doc "Applies `limit` to `queryable`"
+  @spec limit(Ecto.Queryable.t(), non_neg_integer()) :: Ecto.Queryable.t()
+  def limit(queryable, count) when is_integer(count) and count >= 0,
+    do: queryable |> Ecto.Query.limit(^count)
 end
--- test/supra_test.exs
+++ test/supra_test.exs
@@ -1,4 +1,5 @@
 defmodule SupraTest do
+  # @related [subject](lib/supra.ex)
   use Test.DataCase, async: true
   require Ecto.Query
   doctest Supra
@@ -13,6 +14,15 @@ defmodule SupraTest do
       Ecto.Query.from(h in Test.Schemas.House, where: h.address == "123 Main St")
       |> Supra.count(repo: Test.Repo)
       |> assert_eq(1)
+    end
+  end
+
+  describe "limit" do
+    test "applies a limit to a query" do
+      Test.Schemas.House.changeset(address: "123 Main St") |> Test.Repo.insert!()
+      Test.Schemas.House.changeset(address: "234 Main St") |> Test.Repo.insert!()
+
+      assert Test.Schemas.House |> Supra.limit(1) |> Supra.count(repo: Test.Repo) == 1
     end
   end
 end
